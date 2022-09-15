31e2ecdc30add07bb433b12d8f8739ff93aaeb3b
Sarah dePalo <sarahdepalo99@gmail.com>
2022-09-15 16:07:16 -0400

    initial commit. Started playing around with the api. Finished fetching of shiba image

---
+++ get_user.py
@@ -0,0 +1,48 @@
+from requests_oauthlib import OAuth1Session
+import os
+import json
+import tweepy
+import requests
+
+# consumer_key = os.environ.get("CONSUMER_KEY")
+# consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+
+client = tweepy.Client(bearer_token=access_token)
+
+# Get the Shiba Inu image url from the api 
+shiba_api = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true"
+
+try:
+    response = requests.get(shiba_api)
+except:
+    print("error while calling API")
+
+# Parse the results and extract the url and image extension
+data = response.text
+parsed_url = json.loads(data)[0]
+image_extension = parsed_url.split('.')[-1]
+
+# Download the shiba image to later upload as media and tweet
+img_data = requests.get(parsed_url).content
+with open('shiba.' + image_extension, 'wb') as handler:
+    handler.write(img_data)
+
+# Next steps = use twitter API to upload image to twitter and get the returned media_id
+# This media_id will be used to create a tweet
+
+
+
+
+# client.create_tweet(media={"http://shibe.online/api/shibes?count=1&urls=[true/false]&httpsUrls=true"})
+
+
+
+# query = 'dog -is:retweet'
+# file_name = "tweets.txt"
+# if not os.path.exists(file_name):
+#     open(file_name, 'w').close()
+
+# with open(file_name, 'a+') as filehandle:
+#     for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, tweet_fields=['text', 'created_at'], max_results=100).flatten(limit=10):
+#         filehandle.write('%s\n' % tweet.text)
