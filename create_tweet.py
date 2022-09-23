026268c90d5f4fed406f750462039ac566bea8d6
Sarah dePalo <sarahdepalo99@gmail.com>
2022-09-23 16:38:18 -0400

    Script is working to authenticate, upload image, and tweet

--- create_tweet.py
+++ create_tweet.py
@@ -1,12 +1,11 @@
 from requests_oauthlib import OAuth1Session
 import os
 import json
+import sys
+import base64
 
 consumer_key = os.environ.get("CONSUMER_KEY")
 consumer_secret = os.environ.get("CONSUMER_SECRET")
-
-
-payload = {"text": "Hello World! I'm am shiba."}
 
 # Get the request token
 request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
@@ -51,19 +50,117 @@ oauth = OAuth1Session(
     resource_owner_secret=access_token_secret,
 )
 
-# Making the request
+
+
+
+
+
+total_bytes = os.path.getsize("./shiba.jpg");
+print(total_bytes)
+
+
+# INIT
+init_data = {
+     'command': 'INIT',
+      'media_type': 'image/jpg',
+      'total_bytes': total_bytes,
+      'media_category': 'tweet_image'
+}
+init_response = oauth.post(url="https://upload.twitter.com/1.1/media/upload.json", data=init_data)
+media_id = init_response.json()["media_id"]
+
+# UPLOAD
+
+segment_id = 0
+bytes_sent = 0
+file = open("./shiba.jpg", 'rb')
+
+while bytes_sent < total_bytes:
+    chunk = file.read(4*1024*1024)
+    
+    print("APPEND")
+    
+    request_data = {
+        "command": "APPEND",
+        "media_id": media_id,
+        "segment_index": segment_id
+    }
+    
+    files = {
+        "media": chunk
+    }
+    
+    req = oauth.post(url="https://upload.twitter.com/1.1/media/upload.json", data=request_data, files=files)
+    
+    if req.status_code < 200 or req.status_code > 299:
+        print(req.status_code)
+        print(req.text)
+        sys.exit(0)
+
+    segment_id = segment_id + 1
+    bytes_sent = file.tell()
+
+    print('%s of %s bytes uploaded' % (str(bytes_sent), str(total_bytes)))
+
+print('Upload chunks complete.')
+
+
+print('FINALIZE')
+finalize_data = {
+      'command': 'FINALIZE',
+      'media_id': media_id
+    }
+
+req = oauth.post(url="https://upload.twitter.com/1.1/media/upload.jso", data=request_data)
+print(req.json())
+
+processing_info = req.json().get('processing_info', None)
+check_status()
+
+
+
+# publish_data = {
+#     "text": "I am shibe",
+#     "media_ids": media_id
+# }
+
+payload = {
+    "text": "I am shibe",
+    "media": {"media_ids": [f"{media_id}"]}
+}
+
 response = oauth.post(
     "https://api.twitter.com/2/tweets",
     json=payload,
 )
 
-if response.status_code != 201:
-    raise Exception(
-        "Request returned an error: {} {}".format(response.status_code, response.text)
-    )
+print(response.json())
 
-print("Response code: {}".format(response.status_code))
+# with open('./shiba.jpg', 'rb') as img_file:
+#     b64_string = base64.b64encode(img_file.read())
 
-# Saving the response as JSON
-json_response = response.json()
-print(json.dumps(json_response, indent=4, sort_keys=True))
+# payload = {"media_data" :  b64_string.decode('utf-8'),
+#            "media_category": "tweet_image"
+#            }
+
+# response = oauth.post(
+#     "https://upload.twitter.com/1.1/media/upload.json",
+#      json=payload
+# )
+
+# Making the request
+# response = oauth.post(
+#     "https://api.twitter.com/2/tweets",
+#     json=payload,
+# )
+
+# if response.status_code != 201:
+#     raise Exception(
+#         "Request returned an error: {} {}".format(response.status_code, response.text)
+#     )
+
+# print("Response code: {}".format(response.status_code))
+
+# # Saving the response as JSON
+# json_response = response.json()
+# print(json.dumps(json_response, indent=4, sort_keys=True))
