10521f1eb5d3ec5fb1739e798a26d8770411fed3
Sarah dePalo <sarahdepalo99@gmail.com>
2022-09-29 15:42:41 -0400

    created global const file

---
+++ constants.py
@@ -0,0 +1,19 @@
+import os
+
+# File information
+IMAGE_FILENAME = './shiba.jpg'
+ID_FILE = "last_mention_id.txt"
+
+# Bot username
+USERNAME = "daily_shiba_inu"
+
+# Twitter API Endpoints
+MEDIA_UPLOAD_ENDPOINT = 'https://upload.twitter.com/1.1/media/upload.json'
+POST_TWEET_ENDPOINT = 'https://api.twitter.com/1.1/statuses/update.json'
+USERS_ENDPOINT = "https://api.twitter.com/2/users"
+
+# Twitter API vars
+CONSUMER_KEY = os.environ.get("API_KEY")
+CLIENT_SECRET = os.environ.get("API_KEY_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
